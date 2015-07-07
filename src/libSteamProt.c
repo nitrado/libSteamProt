027c91756aded3dddf4ce00232685136bb994f0a
stefan.reusch <stefan.reusch@marbis.net>
2015-07-08 00:13:38 +0200

    added source, udp sample server

---
+++ src/libSteamProt.c
@@ -0,0 +1,547 @@
+#include "libSteamProt.h"
+#include "../bzlib/bzlib.h"
+
+#define safe_string_copy(dst, src) \
+    memset(dst, 0, SP_STRING_LENGTH); \
+    strncpy(dst, src, SP_STRING_LENGTH-1);
+
+#define safe_string_append(dst, src) \
+    { \
+    int StringLength = strnlen(src, SP_STRING_LENGTH); \
+    memcpy(&dst[Pos], src, StringLength); \
+    dst[Pos + StringLength] = 0; \
+    Pos += StringLength + 1; \
+    }
+
+void SP_API WRITE_BUF_setCount(WRITE_BUF* pWriteBuf, unsigned char Count) {
+    pWriteBuf->m_Count = Count;
+}
+
+void SP_API WRITE_BUF_setSize(WRITE_BUF* pWriteBuf, unsigned char Index, short Size) {
+    pWriteBuf->m_Size[Index] = Size;
+}
+
+unsigned char SP_API WRITE_BUF_getCount(WRITE_BUF* pWriteBuf) {
+    return pWriteBuf->m_Count;
+}
+
+short SP_API WRITE_BUF_getSize(WRITE_BUF* pWriteBuf, unsigned char Index) {
+    return pWriteBuf->m_Size[Index];
+}
+
+void SP_API A2S_INFO_setPort(A2S_INFO* pInfo, short Port) {
+    pInfo->m_Port = Port;
+}
+
+short SP_API A2S_INFO_getPort(A2S_INFO* pInfo) {
+    return pInfo->m_Port;
+}
+
+void SP_API A2S_INFO_setSteamID(A2S_INFO* pInfo, long long SteamID) {
+    pInfo->m_SteamID = SteamID;
+}
+
+long long SP_API A2S_INFO_getSteamID(A2S_INFO* pInfo) {
+    return pInfo->m_SteamID;
+}
+
+void SP_API A2S_INFO_setSourceTV_Port(A2S_INFO* pInfo, short Port) {
+    pInfo->m_SourceTV_Port = Port;
+}
+
+short SP_API A2S_INFO_getSourceTV_Port(A2S_INFO* pInfo) {
+    return pInfo->m_SourceTV_Port;
+}
+
+void SP_API A2S_INFO_setSourceTV_Name(A2S_INFO* pInfo, const char* Name) {
+    safe_string_copy(pInfo->m_SourceTV_Name, Name);
+}
+
+const char* SP_API A2S_INFO_getSourceTV_Name(A2S_INFO* pInfo) {
+    return pInfo->m_SourceTV_Name;
+}
+
+void SP_API A2S_INFO_setKeywords(A2S_INFO* pInfo, const char* Keywords) {
+    safe_string_copy(pInfo->m_Keywords, Keywords);
+}
+
+const char* SP_API A2S_INFO_getKeywords(A2S_INFO* pInfo) {
+    return pInfo->m_Keywords;
+}
+
+void SP_API A2S_INFO_setGameID(A2S_INFO* pInfo, long long GameID) {
+    pInfo->m_GameID = GameID;
+}
+
+long long SP_API A2S_INFO_getGameID(A2S_INFO* pInfo) {
+    return pInfo->m_GameID;
+}
+
+void SP_API A2S_INFO_setProtocol(A2S_INFO* pInfo, unsigned char Protocol) {
+    pInfo->m_Protocol = Protocol;
+}
+
+unsigned char SP_API A2S_INFO_getProtocol(A2S_INFO* pInfo) {
+    return pInfo->m_Protocol;
+}
+
+void SP_API A2S_INFO_setName(A2S_INFO* pInfo, const char* Name) {
+    safe_string_copy(pInfo->m_Name, Name);
+}
+
+const char* SP_API A2S_INFO_getName(A2S_INFO* pInfo) {
+    return pInfo->m_Name;
+}
+
+void SP_API A2S_INFO_setMap(A2S_INFO* pInfo, const char* Map) {
+    safe_string_copy(pInfo->m_Map, Map);
+}
+
+const char* SP_API A2S_INFO_getMap(A2S_INFO* pInfo) {
+    return pInfo->m_Map;
+}
+
+void SP_API A2S_INFO_setFolder(A2S_INFO* pInfo, const char* Folder) {
+    safe_string_copy(pInfo->m_Folder, Folder);
+}
+
+const char* SP_API A2S_INFO_getFolder(A2S_INFO* pInfo) {
+    return pInfo->m_Folder;
+}
+
+void SP_API A2S_INFO_setGame(A2S_INFO* pInfo, const char* Game) {
+    safe_string_copy(pInfo->m_Game, Game);
+}
+
+const char* SP_API A2S_INFO_getGame(A2S_INFO* pInfo) {
+    return pInfo->m_Game;
+}
+
+void SP_API A2S_INFO_setID(A2S_INFO* pInfo, short ID) {
+    pInfo->m_ID = ID;
+}
+
+short SP_API A2S_INFO_getID(A2S_INFO* pInfo) {
+    return pInfo->m_ID;
+}
+
+void SP_API A2S_INFO_setPlayers(A2S_INFO* pInfo, unsigned char Players) {
+    pInfo->m_Players = Players;
+}
+
+unsigned char SP_API A2S_INFO_getPlayers(A2S_INFO* pInfo) {
+    return pInfo->m_Players;
+}
+
+void SP_API A2S_INFO_setMaxPlayers(A2S_INFO* pInfo, unsigned char MaxPlayers) {
+    pInfo->m_MaxPlayers = MaxPlayers;
+}
+
+unsigned char SP_API A2S_INFO_getMaxPlayers(A2S_INFO* pInfo) {
+    return pInfo->m_MaxPlayers;
+}
+
+void SP_API A2S_INFO_setBots(A2S_INFO* pInfo, unsigned char Bots) {
+    pInfo->m_Bots = Bots;
+}
+
+unsigned char SP_API A2S_INFO_getBots(A2S_INFO* pInfo) {
+    return pInfo->m_Bots;
+}
+
+void SP_API A2S_INFO_setServertype(A2S_INFO* pInfo, unsigned char Servertype) {
+    pInfo->m_Servertype = Servertype;
+}
+
+unsigned char SP_API A2S_INFO_getServertype(A2S_INFO* pInfo) {
+    return pInfo->m_Servertype;
+}
+
+void SP_API A2S_INFO_setEnvironment(A2S_INFO* pInfo, unsigned char Environment) {
+    pInfo->m_Environment = Environment;
+}
+
+unsigned char SP_API A2S_INFO_getEnvironment(A2S_INFO* pInfo) {
+    return pInfo->m_Environment;
+}
+
+void SP_API A2S_INFO_setVisibility(A2S_INFO* pInfo, unsigned char Visibility) {
+    pInfo->m_Visibility = Visibility;
+}
+
+unsigned char SP_API A2S_INFO_getVisibility(A2S_INFO* pInfo) {
+    return pInfo->m_Visibility;
+}
+
+void SP_API A2S_INFO_setVAC(A2S_INFO* pInfo, unsigned char VAC) {
+    pInfo->m_VAC = VAC;
+}
+
+unsigned char SP_API A2S_INFO_getVAC(A2S_INFO* pInfo) {
+    return pInfo->m_VAC;
+}
+
+void SP_API A2S_INFO_setVersion(A2S_INFO* pInfo, const char* Version) {
+    safe_string_copy(pInfo->m_Version, Version);
+}
+
+const char* SP_API A2S_INFO_getVersion(A2S_INFO* pInfo) {
+    return pInfo->m_Version;
+}
+
+void SP_API A2S_INFO_setEDF(A2S_INFO* pInfo, unsigned char EDF) {
+    pInfo->m_EDF = EDF;
+}
+
+unsigned char SP_API A2S_INFO_getEDF(A2S_INFO* pInfo) {
+    return pInfo->m_EDF;
+}
+
+void SP_API PLAYER_STRUCT_setName(PLAYER_STRUCT* pPlayerStruct, const char* Name) {
+    safe_string_copy(pPlayerStruct->m_Name, Name);
+}
+
+const char* SP_API PLAYER_STRUCT_getName(PLAYER_STRUCT* pPlayerStruct) {
+    return pPlayerStruct->m_Name;
+}
+
+void SP_API PLAYER_STRUCT_setScore(PLAYER_STRUCT* pPlayerStruct, long Score) {
+    pPlayerStruct->m_Score = Score;
+}
+
+long SP_API PLAYER_STRUCT_getScore(PLAYER_STRUCT* pPlayerStruct) {
+    return pPlayerStruct->m_Score;
+}
+
+void SP_API PLAYER_STRUCT_setDuration(PLAYER_STRUCT* pPlayerStruct, float Duration) {
+    pPlayerStruct->m_Duration = Duration;
+}
+
+float SP_API PLAYER_STRUCT_getDuration(PLAYER_STRUCT* pPlayerStruct) {
+    return pPlayerStruct->m_Duration;
+}
+
+void SP_API RULE_STRUCT_setName(RULE_STRUCT* pRuleStruct, const char* Name) {
+    safe_string_copy(pRuleStruct->m_Name, Name);
+}
+
+const char* SP_API RULE_STRUCT_getName(RULE_STRUCT* pRuleStruct) {
+    return pRuleStruct->m_Name;
+}
+
+void SP_API RULE_STRUCT_setValue(RULE_STRUCT* pRuleStruct, const char* Value) {
+    safe_string_copy(pRuleStruct->m_Value, Value);
+}
+
+const char* SP_API RULE_STRUCT_getValue(RULE_STRUCT* pRuleStruct) {
+    return pRuleStruct->m_Value;
+}
+
+void SP_API A2S_RULES_setRuleCount(A2S_RULES* pRules, short RuleCount) {
+    pRules->m_RuleCount = RuleCount;
+}
+
+short SP_API A2S_RULES_getRuleCount(A2S_RULES* pRules) {
+    return pRules->m_RuleCount;
+}
+
+void SP_API A2S_PLAYER_setPlayerCount(A2S_PLAYER* pPlayer, unsigned char PlayerCount) {
+    pPlayer->m_PlayerCount = PlayerCount;
+}
+
+unsigned char SP_API A2S_PLAYER_getPlayerCount(A2S_PLAYER* pPlayer) {
+    return pPlayer->m_PlayerCount;
+}
+
+void SP_API SPLIT_STRUCT_setID(SPLIT_STRUCT* pSplitStruct, long ID) {
+    pSplitStruct->m_ID |= (ID & 0x7FFFFFFFF);
+}
+
+void SP_API SPLIT_STRUCT_setCompressed(SPLIT_STRUCT* pSplitStruct) {
+    pSplitStruct->m_ID |= 0x80000000;
+}
+
+unsigned char SP_API SPLIT_STRUCT_isCompressed(SPLIT_STRUCT* pSplitStruct) {
+    return !!(pSplitStruct->m_ID & 0x80000000);
+}
+
+long SP_API SPLIT_STRUCT_getID(SPLIT_STRUCT* pSplitStruct) {
+    return pSplitStruct->m_ID;
+}
+
+void SP_API SPLIT_STRUCT_setTotal(SPLIT_STRUCT* pSplitStruct, unsigned char Total) {
+    pSplitStruct->m_Total = Total;
+}
+
+unsigned char SP_API SPLIT_STRUCT_getTotal(SPLIT_STRUCT* pSplitStruct) {
+    return pSplitStruct->m_Total;
+}
+
+unsigned char SP_API SPLIT_STRUCT_getNumber(SPLIT_STRUCT* pSplitStruct) {
+    return pSplitStruct->m_Number;
+}
+
+short SP_API SPLIT_STRUCT_getSize(SPLIT_STRUCT* pSplitStruct) {
+    return pSplitStruct->m_Size;
+}
+
+void SP_API SPLIT_STRUCT_setNumber(SPLIT_STRUCT* pSplitStruct, unsigned char Number) {
+    pSplitStruct->m_Number = Number;
+}
+
+void SP_API SPLIT_STRUCT_setSize(SPLIT_STRUCT* pSplitStruct, short Size) {
+    pSplitStruct->m_Size = Size;
+}
+
+void SP_API WRITE_BUF_INIT(WRITE_BUF* pWriteBuf) {
+    memset(pWriteBuf, 0, sizeof(WRITE_BUF));
+}
+
+void SP_API A2S_RULES_INIT(A2S_RULES* pRules) {
+    memset(pRules, 0, sizeof(A2S_RULES));
+}
+
+void SP_API SPLIT_STRUCT_INIT(SPLIT_STRUCT* pSplit) {
+    memset(pSplit, 0, sizeof(SPLIT_STRUCT));
+}
+
+int SP_API SPLIT_STRUCT_WRITE(SPLIT_STRUCT* pSplit, unsigned char* pBuf, int* pBufLen) {
+    int Pos = 0;
+    long Header, ID;
+    short Size;
+
+    Header = htonl(0xFEFFFFFF);
+    memcpy(&pBuf[Pos], &Header, sizeof(Header));
+    Pos += 4;
+    ID = SPLIT_STRUCT_getID(pSplit);
+    memcpy(&pBuf[Pos], &ID, sizeof(ID));
+    Pos += 4;
+    pBuf[Pos++] = SPLIT_STRUCT_getTotal(pSplit);
+    pBuf[Pos++] = SPLIT_STRUCT_getNumber(pSplit);
+    if(!SPLIT_STRUCT_isCompressed(pSplit)) {
+        Size = SPLIT_STRUCT_getSize(pSplit);
+        memcpy(&pBuf[Pos], &Size, sizeof(Size));
+        Pos += 2;
+    }
+
+    *pBufLen = Pos;
+
+    return STEAMPROT_OK;
+}
+
+extern unsigned int crc32(unsigned int crc, const void *buf, unsigned int size);
+
+int SP_API A2S_RULES_WRITE(A2S_RULES* pRules, WRITE_BUF* pWriteBuf) {
+    unsigned char pBuf[SP_MAX_PACKET_SIZE], j;
+    int Pos, BufLen, NewBufLen, BufLenLeft;
+    long Header, DecompressedSize;
+    short RuleCount, i;
+    unsigned char* pPacketBuf;
+#if SP_COMPRESSED
+    bz_stream BZStream;
+    unsigned char CompressedBuf[SP_MAX_PACKET_SIZE];
+    unsigned int CompressedSize, CRC;
+#endif
+    /*FILE* fp;
+    char FILENAME[255];*/
+
+    Pos = 0;
+    memset(pBuf, 0, SP_MAX_PACKET_SIZE);
+    Header = htonl(0xFFFFFFFF);
+    memcpy(&pBuf[Pos], &Header, sizeof(Header));
+    Pos += 4;
+    pBuf[Pos++] = SP_RESPONSE_RULES;
+    RuleCount = A2S_RULES_getRuleCount(pRules);
+    memcpy(&pBuf[Pos], &RuleCount, sizeof(RuleCount));
+    Pos += 2;
+
+    for(i = 0; i < A2S_RULES_getRuleCount(pRules); i++) {
+        RULE_STRUCT* pRuleStruct = &pRules->m_Rules[i];
+        safe_string_append(pBuf, RULE_STRUCT_getName(pRuleStruct));
+        safe_string_append(pBuf, RULE_STRUCT_getValue(pRuleStruct));
+    }
+
+    if(Pos > SP_SPLIT_SIZE) {
+        SPLIT_STRUCT split;
+        SPLIT_STRUCT_INIT(&split);
+        SPLIT_STRUCT_setSize(&split, SP_SPLIT_SIZE);
+        SPLIT_STRUCT_setID(&split, rand());
+#if SP_COMPRESSED
+        if(Pos > SP_COMPRESSED_MIN_SIZE) {
+            CompressedSize = sizeof(CompressedBuf);
+            if(BZ2_bzBuffToBuffCompress(CompressedBuf, &CompressedSize, pBuf, Pos, 9, 0, 30) == BZ_OK) {
+                DecompressedSize = Pos;
+                CRC = crc32(0, pBuf, Pos);
+                memcpy(&pBuf[0], &DecompressedSize, sizeof(DecompressedSize));
+                memcpy(&pBuf[4], &CRC, sizeof(CRC));
+                memcpy(&pBuf[8], &CompressedBuf, CompressedSize);
+                Pos = CompressedSize + 8;
+                SPLIT_STRUCT_setCompressed(&split);
+            }
+        }
+#endif
+        SPLIT_STRUCT_setTotal(&split, (unsigned char)ceilf((float)Pos / SP_SPLIT_SIZE));
+        BufLen = 0, NewBufLen = 0;
+
+        for(j = 0; j < SPLIT_STRUCT_getTotal(&split); j++) {
+            pPacketBuf = pWriteBuf->m_Buffer[j];
+            SPLIT_STRUCT_setNumber(&split, j);
+            SPLIT_STRUCT_WRITE(&split, pPacketBuf, &BufLen);
+                
+            BufLenLeft = Pos - SPLIT_STRUCT_getNumber(&split) * SP_SPLIT_SIZE; 
+            memcpy(&pPacketBuf[BufLen], &pBuf[j*SP_SPLIT_SIZE], (BufLenLeft <= SP_SPLIT_SIZE?BufLenLeft:SP_SPLIT_SIZE));
+            pWriteBuf->m_Size[j] = (BufLenLeft <= SP_SPLIT_SIZE?BufLenLeft:SP_SPLIT_SIZE) + BufLen;
+            
+            /*sprintf(FILENAME, "PACKET_%d.bin", j);
+            fp = fopen(FILENAME, "wb");
+            fwrite(pPacketBuf, pWriteBuf->m_Size[j], 1, fp);
+            fclose(fp);*/
+        }
+
+        WRITE_BUF_setCount(pWriteBuf, SPLIT_STRUCT_getTotal(&split));
+    } else {
+        memcpy(pBuf, pWriteBuf->m_Buffer[0], Pos);
+        pWriteBuf->m_Size[0] = Pos;
+    }
+
+    return STEAMPROT_OK;
+}
+
+void SP_API A2S_PLAYER_INIT(A2S_PLAYER* pPlayer) {
+    memset(pPlayer, 0, sizeof(A2S_PLAYER));
+}
+
+int SP_API A2S_PLAYER_WRITE(A2S_PLAYER* pPlayer, WRITE_BUF* pWriteBuf) {
+    int Pos;
+    unsigned char* pBuf;
+    unsigned char i;
+    long Header, Score;
+    float Duration;
+
+    Pos = 0;
+    pBuf = pWriteBuf->m_Buffer[0];
+    Header = htonl(0xFFFFFFFF);
+    memcpy(&pBuf[Pos], &Header, sizeof(Header));
+    Pos += 4;
+    pBuf[Pos++] = SP_RESPONSE_PLAYER;
+    pBuf[Pos++] = A2S_PLAYER_getPlayerCount(pPlayer);
+    for(i = 0; i < A2S_PLAYER_getPlayerCount(pPlayer); i++) {
+        PLAYER_STRUCT* pPlayerStruct = &pPlayer->m_Players[i];
+        pBuf[Pos++] = i;
+        safe_string_append(pBuf, PLAYER_STRUCT_getName(pPlayerStruct));
+        Score = PLAYER_STRUCT_getScore(pPlayerStruct);
+        memcpy(&pBuf[Pos], &Score, sizeof(Score));
+        Pos += 4;
+        Duration = PLAYER_STRUCT_getDuration(pPlayerStruct);
+        memcpy(&pBuf[Pos], &Duration, sizeof(Duration));
+        Pos += 4;
+    }
+    pWriteBuf->m_Size[0] = Pos;
+    WRITE_BUF_setCount(pWriteBuf, 1);
+
+    return STEAMPROT_OK;
+}
+
+void SP_API A2S_INFO_INIT(A2S_INFO* pInfo) {
+    memset(pInfo, 0, sizeof(A2S_INFO));
+}
+
+int SP_API A2S_INFO_WRITE(A2S_INFO* pInfo, WRITE_BUF* pWriteBuf) {
+    int Pos;
+    long Header;
+    unsigned char* pBuf;
+    short ID, Port, SourceTV_Port;
+    long long SteamdID, GameID;
+
+    pBuf = pWriteBuf->m_Buffer[0];
+
+    Pos = 0;
+    Header = htonl(0xFFFFFFFF);
+    memcpy(&pBuf[Pos], &Header, sizeof(Header));
+    Pos += 4;
+    pBuf[Pos++] = SP_RESPONSE_INFO;
+    pBuf[Pos++] = A2S_INFO_getProtocol(pInfo);
+    safe_string_append(pBuf, A2S_INFO_getName(pInfo));
+    safe_string_append(pBuf, A2S_INFO_getMap(pInfo));
+    safe_string_append(pBuf, A2S_INFO_getFolder(pInfo));
+    safe_string_append(pBuf, A2S_INFO_getGame(pInfo));
+    ID = A2S_INFO_getID(pInfo);
+    memcpy(&pBuf[Pos], &ID, sizeof(ID));
+    Pos += 2;
+    pBuf[Pos++] = A2S_INFO_getPlayers(pInfo);
+    pBuf[Pos++] = A2S_INFO_getMaxPlayers(pInfo);
+    pBuf[Pos++] = A2S_INFO_getBots(pInfo);
+    pBuf[Pos++] = A2S_INFO_getServertype(pInfo);
+    pBuf[Pos++] = A2S_INFO_getEnvironment(pInfo);
+    pBuf[Pos++] = A2S_INFO_getVisibility(pInfo);
+    pBuf[Pos++] = A2S_INFO_getVAC(pInfo);
+    safe_string_append(pBuf, A2S_INFO_getVersion(pInfo));
+    pBuf[Pos++] = A2S_INFO_getEDF(pInfo);
+    if(A2S_INFO_getEDF(pInfo) & SP_EDF_PORT) {
+        Port = A2S_INFO_getPort(pInfo);
+        memcpy(&pBuf[Pos], &Port, sizeof(Port));
+        Pos += 2;
+    }
+    if(A2S_INFO_getEDF(pInfo) & SP_EDF_STEAMID) {
+        SteamdID = A2S_INFO_getSteamID(pInfo);
+        memcpy(&pBuf[Pos], &SteamdID, sizeof(SteamdID));
+        Pos += 8;
+    }
+    if(A2S_INFO_getEDF(pInfo) & SP_EDF_SOURCETV) {
+        SourceTV_Port = A2S_INFO_getSourceTV_Port(pInfo);
+        memcpy(&pBuf[Pos], &SourceTV_Port, sizeof(SourceTV_Port));
+        Pos += 2;
+        safe_string_append(pBuf, A2S_INFO_getSourceTV_Name(pInfo));
+    }
+    if(A2S_INFO_getEDF(pInfo) & SP_EDF_KEYWORDS) {
+        safe_string_append(pBuf, A2S_INFO_getKeywords(pInfo));
+    }
+    if(A2S_INFO_getEDF(pInfo) & SP_EDF_GAMEID) {
+        GameID = A2S_INFO_getGameID(pInfo);
+        memcpy(&pBuf[Pos], &GameID, sizeof(GameID));
+        Pos += 8;
+    }
+
+    pWriteBuf->m_Size[0] = Pos;
+    WRITE_BUF_setCount(pWriteBuf, 1);
+
+    return STEAMPROT_OK;
+}
+
+int SP_API HandlePacket(WRITE_BUF* pWriteBuf, unsigned char* pBuf, int BufLen, SP_Protocol_Callbacks Callbacks) {
+    int Pos;
+    long Header;
+    unsigned char Command;
+
+    if(BufLen < 5) {
+        return STEAMPROT_ERROR;
+    }
+
+    Pos = 0;
+    Header = 0xFFFFFFFF;
+    if(memcmp(&pBuf[Pos], &Header, sizeof(Header)) == 0) {
+        Pos += 4;
+        Command = pBuf[Pos];
+        switch(Command) {
+        case SP_REQUEST_INFO:
+            if(Callbacks.FillInfo == NULL)
+                return STEAMPROT_ERROR;
+            Callbacks.FillInfo(pWriteBuf);
+            return STEAMPROT_OK;
+        case SP_REQUEST_PLAYER:
+            if(Callbacks.FillPayer == NULL)
+                return STEAMPROT_ERROR;
+            Callbacks.FillPayer(pWriteBuf);
+            return STEAMPROT_OK;
+        case SP_REQUEST_RULES:
+            if(Callbacks.FillRules == NULL)
+                return STEAMPROT_ERROR;
+            Callbacks.FillRules(pWriteBuf);
+            return STEAMPROT_OK;
+        default:
+            return STEAMPROT_ERROR;
+        }
+    } else {
+        return STEAMPROT_ERROR;
+    }
+}
---
+++ src/libSteamProt.h
@@ -0,0 +1,254 @@
+#ifndef __LIBSTEAMPROT__
+#define __LIBSTEAMPROT__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(WIN32)
+#include <Windows.h>
+#define SP_API WINAPI
+#define SP_EXTERN extern
+#else
+#define SP_API
+#define SP_EXTERN extern
+#endif
+
+#define SP_PLAYER_MAX            64
+#define SP_RULE_MAX              255
+#define SP_STRING_LENGTH         255
+#define SP_MAX_PACKET_SIZE       16384
+#define SP_SPLIT_SIZE            1248
+#define STEAMPROT_OK             0
+#define STEAMPROT_ERROR          -1
+
+#define SP_SERVERTYPE_DEDICATED  'd'
+#define SP_SERVERTYPE_LISTEN     'l'
+#define SP_SERVERTYPE_P          'p'
+
+#define SP_ENVIRONMENT_LINUX     'l'
+#define SP_ENVIRONMENT_WINDOWS   'w'
+#define SP_ENVIRONMENT_MAC       'm'
+
+#define SP_REQUEST_RULES         'V'
+#define SP_RESPONSE_RULES        'E'
+#define SP_REQUEST_INFO          'T'
+#define SP_RESPONSE_INFO         'I'
+#define SP_REQUEST_PLAYER        'U'
+#define SP_RESPONSE_PLAYER       'D'
+
+#define SP_EDF_GAMEID            0x01
+#define SP_EDF_STEAMID           0x10
+#define SP_EDF_KEYWORDS          0x20
+#define SP_EDF_SOURCETV          0x40
+#define SP_EDF_PORT              0x80
+
+#ifndef SP_COMPRESSED
+#define SP_COMPRESSED 0
+#endif
+
+#ifndef SP_COMPRESSED_MIN_SIZE
+#define SP_COMPRESSED_MIN_SIZE   SP_SPLIT_SIZE * 2
+#endif
+
+typedef struct {
+    unsigned char m_Buffer[16][SP_MAX_PACKET_SIZE];
+    short m_Size[16];
+    unsigned char m_Count;
+
+    //short m_Size;
+} WRITE_BUF;
+
+typedef void (*FillPlayerCallback)(WRITE_BUF* pWriteBuf);
+typedef void (*FillInfoCallback)(WRITE_BUF* pWriteBuf);
+typedef void (*FillRulesCallback)(WRITE_BUF* pWriteBuf);
+
+typedef struct {
+    FillInfoCallback FillInfo;
+    FillPlayerCallback FillPayer;
+    FillRulesCallback FillRules;
+} SP_Protocol_Callbacks;
+
+typedef struct {
+    //unsigned char Header;
+    unsigned char m_Protocol;
+    char m_Name[SP_STRING_LENGTH];
+    char m_Map[SP_STRING_LENGTH];
+    char m_Folder[SP_STRING_LENGTH];
+    char m_Game[SP_STRING_LENGTH];
+    short m_ID;
+    unsigned char m_Players;
+    unsigned char m_MaxPlayers;
+    unsigned char m_Bots;
+    unsigned char m_Servertype;
+    unsigned char m_Environment;
+    unsigned char m_Visibility;
+    unsigned char m_VAC;
+    char m_Version[SP_STRING_LENGTH];
+    unsigned char m_EDF;
+
+    // Extra Data
+    short m_Port;
+    long long m_SteamID;
+    short m_SourceTV_Port;
+    char m_SourceTV_Name[SP_STRING_LENGTH];
+    char m_Keywords[SP_STRING_LENGTH];
+    long long m_GameID;
+} A2S_INFO;
+
+typedef struct {
+    unsigned char m_Index;
+    char m_Name[SP_STRING_LENGTH];
+    long m_Score;
+    float m_Duration;
+} PLAYER_STRUCT;
+
+typedef struct {
+    //unsigned char Header;
+    unsigned char m_PlayerCount;
+    PLAYER_STRUCT m_Players[SP_PLAYER_MAX];
+} A2S_PLAYER;
+
+typedef struct {
+    char m_Name[SP_STRING_LENGTH];
+    char m_Value[SP_STRING_LENGTH];
+} RULE_STRUCT;
+
+typedef struct {
+    //unsigned char Header;
+    short m_RuleCount;
+    RULE_STRUCT m_Rules[SP_RULE_MAX];
+} A2S_RULES;
+
+typedef struct {
+    long m_ID;
+    unsigned char m_Total;
+    unsigned char m_Number;
+    short m_Size;
+} SPLIT_STRUCT;
+
+/*
+    A2S_PLAYER Functions
+*/
+SP_EXTERN void SP_API A2S_PLAYER_INIT(A2S_PLAYER* pPlayer);
+SP_EXTERN int SP_API A2S_PLAYER_WRITE(A2S_PLAYER* pPlayer, WRITE_BUF* pWriteBuf);
+
+SP_EXTERN void SP_API PLAYER_STRUCT_setName(PLAYER_STRUCT* pPlayerStruct, const char* Name);
+SP_EXTERN void SP_API PLAYER_STRUCT_setScore(PLAYER_STRUCT* pPlayerStruct, long Score);
+SP_EXTERN void SP_API PLAYER_STRUCT_setDuration(PLAYER_STRUCT* pPlayerStruct, float Duration);
+SP_EXTERN void SP_API A2S_PLAYER_setPlayerCount(A2S_PLAYER* pPlayer, unsigned char PlayerCount);
+
+SP_EXTERN const char* SP_API PLAYER_STRUCT_getName(PLAYER_STRUCT* pPlayerStruct);
+SP_EXTERN long SP_API PLAYER_STRUCT_getScore(PLAYER_STRUCT* pPlayerStruct);
+SP_EXTERN float SP_API PLAYER_STRUCT_getDuration(PLAYER_STRUCT* pPlayerStruct);
+SP_EXTERN unsigned char SP_API A2S_PLAYER_getPlayerCount(A2S_PLAYER* pPlayer);
+
+/*
+    A2S_RULES Functions
+*/
+SP_EXTERN void SP_API A2S_RULES_INIT(A2S_RULES* pRules);
+SP_EXTERN int SP_API A2S_RULES_WRITE(A2S_RULES* pRules, WRITE_BUF* pWriteBuf);
+
+SP_EXTERN void SP_API RULE_STRUCT_setName(RULE_STRUCT* pRuleStruct, const char* Name);
+SP_EXTERN void SP_API RULE_STRUCT_setValue(RULE_STRUCT* pRuleStruct, const char* Value);
+SP_EXTERN void SP_API A2S_RULES_setRuleCount(A2S_RULES* pRules, short RuleCount);
+
+SP_EXTERN const char* SP_API RULE_STRUCT_getName(RULE_STRUCT* pRuleStruct);
+SP_EXTERN const char* SP_API RULE_STRUCT_getValue(RULE_STRUCT* pRuleStruct);
+SP_EXTERN short SP_API A2S_RULES_getRuleCount(A2S_RULES* pRules);
+
+/*
+    A2S_INFO Functions
+*/
+SP_EXTERN void SP_API A2S_INFO_INIT(A2S_INFO* pInfo);
+SP_EXTERN int SP_API A2S_INFO_WRITE(A2S_INFO* pInfo, WRITE_BUF* pWriteBuf);
+
+SP_EXTERN void SP_API A2S_INFO_setProtocol(A2S_INFO* pInfo, unsigned char Protocol);
+SP_EXTERN void SP_API A2S_INFO_setPort(A2S_INFO* pInfo, short Port);
+SP_EXTERN void SP_API A2S_INFO_setSteamID(A2S_INFO* pInfo, long long SteamID);
+SP_EXTERN void SP_API A2S_INFO_setSourceTV_Port(A2S_INFO* pInfo, short Port);
+SP_EXTERN void SP_API A2S_INFO_setSourceTV_Name(A2S_INFO* pInfo, const char* Name);
+SP_EXTERN void SP_API A2S_INFO_setKeywords(A2S_INFO* pInfo, const char* Keywords);
+SP_EXTERN void SP_API A2S_INFO_setGameID(A2S_INFO* pInfo, long long GameID);
+SP_EXTERN void SP_API A2S_INFO_setProtocol(A2S_INFO* pInfo, unsigned char Protocol);
+SP_EXTERN void SP_API A2S_INFO_setName(A2S_INFO* pInfo, const char* Name);
+SP_EXTERN void SP_API A2S_INFO_setMap(A2S_INFO* pInfo, const char* Map);
+SP_EXTERN void SP_API A2S_INFO_setFolder(A2S_INFO* pInfo, const char* Folder);
+SP_EXTERN void SP_API A2S_INFO_setGame(A2S_INFO* pInfo, const char* Game);
+SP_EXTERN void SP_API A2S_INFO_setID(A2S_INFO* pInfo, short ID);
+SP_EXTERN void SP_API A2S_INFO_setPlayers(A2S_INFO* pInfo, unsigned char Players);
+SP_EXTERN void SP_API A2S_INFO_setMaxPlayers(A2S_INFO* pInfo, unsigned char MaxPlayers);
+SP_EXTERN void SP_API A2S_INFO_setBots(A2S_INFO* pInfo, unsigned char Bots);
+SP_EXTERN void SP_API A2S_INFO_setServertype(A2S_INFO* pInfo, unsigned char Servertype);
+SP_EXTERN void SP_API A2S_INFO_setEnvironment(A2S_INFO* pInfo, unsigned char Environment);
+SP_EXTERN void SP_API A2S_INFO_setVisibility(A2S_INFO* pInfo, unsigned char Visibility);
+SP_EXTERN void SP_API A2S_INFO_setVAC(A2S_INFO* pInfo, unsigned char VAC);
+SP_EXTERN void SP_API A2S_INFO_setVersion(A2S_INFO* pInfo, const char* Version);
+SP_EXTERN void SP_API A2S_INFO_setEDF(A2S_INFO* pInfo, unsigned char EDF);
+
+SP_EXTERN short SP_API A2S_INFO_getPort(A2S_INFO* pInfo);
+SP_EXTERN long long SP_API A2S_INFO_getSteamID(A2S_INFO* pInfo);
+SP_EXTERN short SP_API A2S_INFO_getSourceTV_Port(A2S_INFO* pInfo);
+SP_EXTERN const char* SP_API A2S_INFO_getSourceTV_Name(A2S_INFO* pInfo);
+SP_EXTERN const char* SP_API A2S_INFO_getKeywords(A2S_INFO* pInfo);
+SP_EXTERN long long SP_API A2S_INFO_getGameID(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getProtocol(A2S_INFO* pInfo);
+SP_EXTERN const char* SP_API A2S_INFO_getName(A2S_INFO* pInfo);
+SP_EXTERN const char* SP_API A2S_INFO_getMap(A2S_INFO* pInfo);
+SP_EXTERN const char* SP_API A2S_INFO_getFolder(A2S_INFO* pInfo);
+SP_EXTERN const char* SP_API A2S_INFO_getGame(A2S_INFO* pInfo);
+SP_EXTERN short SP_API A2S_INFO_getID(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getPlayers(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getMaxPlayers(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getBots(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getServertype(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getEnvironment(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getVisibility(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getVAC(A2S_INFO* pInfo);
+SP_EXTERN const char* SP_API A2S_INFO_getVersion(A2S_INFO* pInfo);
+SP_EXTERN unsigned char SP_API A2S_INFO_getEDF(A2S_INFO* pInfo);
+
+/*
+    SPLIT_STRUCT Functions
+*/
+SP_EXTERN void SP_API SPLIT_STRUCT_INIT(SPLIT_STRUCT* pSplit);
+SP_EXTERN int SP_API SPLIT_STRUCT_WRITE(SPLIT_STRUCT* pSplit, unsigned char* pBuf, int* pBufLen);
+
+SP_EXTERN void SP_API SPLIT_STRUCT_setID(SPLIT_STRUCT* pSplitStruct, long ID);
+SP_EXTERN void SP_API SPLIT_STRUCT_setCompressed(SPLIT_STRUCT* pSplitStruct);
+SP_EXTERN void SP_API SPLIT_STRUCT_setTotal(SPLIT_STRUCT* pSplitStruct, unsigned char Total);
+SP_EXTERN void SP_API SPLIT_STRUCT_setNumber(SPLIT_STRUCT* pSplitStruct, unsigned char Number);
+SP_EXTERN void SP_API SPLIT_STRUCT_setSize(SPLIT_STRUCT* pSplitStruct, short Size);
+
+SP_EXTERN unsigned char SP_API SPLIT_STRUCT_isCompressed(SPLIT_STRUCT* pSplitStruct);
+SP_EXTERN long SP_API SPLIT_STRUCT_getID(SPLIT_STRUCT* pSplitStruct);
+SP_EXTERN unsigned char SP_API SPLIT_STRUCT_getTotal(SPLIT_STRUCT* pSplitStruct);
+SP_EXTERN unsigned char SP_API SPLIT_STRUCT_getNumber(SPLIT_STRUCT* pSplitStruct);
+SP_EXTERN short SP_API SPLIT_STRUCT_getSize(SPLIT_STRUCT* pSplitStruct);
+
+/*
+    WRITE_BUF Functions
+*/
+SP_EXTERN void SP_API WRITE_BUF_INIT(WRITE_BUF* pWriteBuf);
+
+SP_EXTERN void SP_API WRITE_BUF_setCount(WRITE_BUF* pWriteBuf, unsigned char Count);
+SP_EXTERN void SP_API WRITE_BUF_setSize(WRITE_BUF* pWriteBuf, unsigned char Index, short Size);
+
+SP_EXTERN unsigned char SP_API WRITE_BUF_getCount(WRITE_BUF* pWriteBuf);
+SP_EXTERN short SP_API WRITE_BUF_getSize(WRITE_BUF* pWriteBuf, unsigned char Index);
+
+/*
+    Handle Socket Data
+*/
+SP_EXTERN int SP_API HandlePacket(WRITE_BUF* pWriteBuf, unsigned char* pBuf, int BufLen, SP_Protocol_Callbacks Callbacks);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __LIBSTEAMPROT__
